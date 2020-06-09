b3fc1d8be3435e92ef8618b76e91e29c654f5d37
masaya-tn <uvermasayan@icloud.com>
2020-06-09 17:58:57 +0900

    プロフィール

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,6 +23,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :articles, dependent: :destroy
+  has_one :profile, dependent: :destroy
+
+  delegate :birthday, :age, :gender, to: :profile, allow_nil: true
 
   def has_written?(article)
     articles.exists?(id: article.id)
@@ -30,7 +33,22 @@ class User < ApplicationRecord
 
   
   def display_name
-    self.email.split('@').first
+    # ボッチ演算子
+    profile&.nickname || self.email.split('@').first
+  end
+
+  
+
+  def prepare_profile
+    profile || build_profile
+  end
+
+  def avatar_image
+    if profile&.avatar&.attached?
+      profile.avatar
+    else
+      'default-avatar.png'
+    end
   end
   
 end
